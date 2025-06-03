ba588384d6de2f8fbd98bd284ae55d751c3affdb
Josivan Reis <josivan.reis@gruposetup.com>
2025-06-03 11:40:43 -0300

    Minha alteração

---
+++ next.config.js
@@ -0,0 +1,14 @@
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['localhost'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+}
+
+module.exports = nextConfig
